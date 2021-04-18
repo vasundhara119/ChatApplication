6b330d9e0918caf781e95f7b4f3d87279c87ce76
P Avinash Rao <podili.avinash.rao@sap.com>
2021-04-18 17:58:10 +0530

    Initial Commit

---
+++ src/main/java/com/assignment/oopd/OopdApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.oopd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopdApplication.class, args);
+	}
+
+}
