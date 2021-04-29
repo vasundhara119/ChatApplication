0ab6e4000980e1a0937a2d7931b22e2814b654e6
P Avinash Rao <podili.avinash.rao@sap.com>
2021-04-29 12:30:00 +0530

    Commiting skip gitignore

---
+++ target/classes/static/js/main.js
@@ -0,0 +1,134 @@
+"use strict";
+
+var usernamePage = document.querySelector("#username-page");
+var chatPage = document.querySelector("#chat-page");
+var createRoomForm = document.querySelector("#createRoomForm");
+var joinRoomForm = document.querySelector("#joinRoomForm");
+var messageForm = document.querySelector("#messageForm");
+var messageInput = document.querySelector("#message");
+var messageArea = document.querySelector("#messageArea");
+var connectingElement = document.querySelector(".connecting");
+
+var stompClient = null;
+var username = null;
+let roomId = null;
+
+var colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
+function randomNumberGenerator(min, max) {
+    return Math.floor((Math.random() * max) + min);
+}
+
+function createRoom(event) {
+    event.preventDefault();
+    username = document.querySelector("#name-to-create").value.trim();
+    roomId = randomNumberGenerator(1, 100);
+    connect();
+}
+
+function joinRoom(event) {
+    event.preventDefault();
+    username = document.querySelector("#name-to-join").value.trim();
+    roomId = document.querySelector("#roomid").value.trim();
+    connect();
+}
+
+function connect(event) {
+
+    if (username) {
+        document.querySelector("#chatpage-roomid").innerHTML = roomId;
+
+        usernamePage.classList.add("hidden");
+        chatPage.classList.remove("hidden");
+
+        var socket = new SockJS("/ws");
+        stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, onConnected, onError);
+    }
+    event.preventDefault();
+}
+
+function onConnected() {
+    //subscribe to the public topic
+    stompClient.subscribe("/topic/" + roomId, onMessageReceived);
+    console.log("subscribe function worked hopefully");
+
+    //Tell your name to the server
+    stompClient.send("/app/chat.addUser", {},
+        JSON.stringify({ sender: username, type: "JOIN", roomId: roomId })
+    )
+    connectingElement.classList.add("hidden");
+}
+
+function onError() {
+    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+    connectingElement.style.color = 'red';
+}
+
+function sendMessage(event) {
+    var messageContent = messageInput.value.trim();
+    if (messageContent && stompClient) {
+        var chatMessage = {
+            sender: username,
+            content: messageInput.value,
+            type: 'CHAT',
+            roomId: roomId
+        };
+        stompClient.send("/app/chat.sendMessage/" + roomId, {}, JSON.stringify(chatMessage));
+        messageInput.value = "";
+    }
+    event.preventDefault();
+}
+
+function onMessageReceived(payload) {
+    var message = JSON.parse(payload.body);
+    var messageElement = document.createElement('li');
+
+    if (message.type === 'JOIN') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' joined!';
+    } else if (message.type === 'LEAVE') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' left!';
+    } else {
+        messageElement.classList.add('chat-message');
+
+        var avatarElement = document.createElement('i');
+        var avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        var usernameElement = document.createElement('span');
+        var usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+function getAvatarColor(messageSender) {
+    var hash = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+    var index = Math.abs(hash % colors.length);
+    return colors[index];
+}
+
+createRoomForm.addEventListener("submit", createRoom, true);
+joinRoomForm.addEventListener("submit", joinRoom, true);
+messageForm.addEventListener("submit", sendMessage, true);
---
+++ target/classes/static3/Chat window.js
@@ -0,0 +1,61 @@
+Message = function(arg) {
+    this.text = arg.text, this.message_side = arg.message_side;
+    this.draw = function(_this) {
+        return function() {
+            var $message;
+            // console.log("hello!"); 
+            $message = $($('.message_template').clone().html());
+            $message.addClass(_this.message_side).find('.text').html(_this.text);
+            $('.messages').append($message);
+            return setTimeout(function() {
+                return $message.addClass('appeared');
+            }, 0);
+        };
+    }(this);
+    return this;
+};
+
+
+$(function() {
+    // var username = document.getElementById("username").value;
+    // console.log(username); 
+    var getMessageText, message_side, sendMessage;
+    message_side = 'right';
+    getMessageText = function() {
+        var $message_input;
+        $message_input = $('.message_input');
+        return $message_input.val();
+    };
+    sendMessage = function(text) {
+        var $messages, message;
+        if (text.trim() === '') {
+            return;
+        }
+        $('.message_input').val('');
+        $messages = $('.messages');
+        message_side = message_side === 'left' ? 'right' : 'left';
+        message = new Message({
+            text: text,
+            message_side: message_side
+        });
+        message.draw();
+        return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+    };
+    $('.send_message').click(function(e) {
+        return sendMessage(getMessageText());
+    });
+    $('.message_input').keyup(function(e) {
+        if (e.which === 13) {
+            console.log(getMessageText);
+            return sendMessage(getMessageText());
+        }
+    });
+    //you can remove this .... it was added just for trial
+    // sendMessage('Hi Aditi!');
+    // setTimeout(function() {
+    //     return sendMessage('Hi Pooja');
+    // }, 1000);
+    // return setTimeout(function() {
+    //     return sendMessage('Hi everyone!');
+    // }, 2000);
+});
